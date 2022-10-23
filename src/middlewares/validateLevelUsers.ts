143119e1f59fe9785698564bfb7632a7ab1196fa
Bruna Santiago <brunasanthiago@outlook.com>
2022-10-23 15:01:06 -0300

    agora vai o requisito 7

--- src/middlewares/validateLevelUsers.ts
+++ src/middlewares/validateLevelUsers.ts
@@ -3,16 +3,14 @@ import { Request, Response, NextFunction } from 'express';
 export default function validateLevelUsers(req: Request, res: Response, next: NextFunction) {
   const { level } = req.body;
         
-  if (!level) {
+  if (level === undefined || level === null) {
     return res.status(400).json({ message: '"level" is required' });
+  }
+  if (level < 1) {
+    return res.status(422).json({ message: '"level" must be greater than or equal to 1' });
   }
   if (typeof level !== 'number') {
     return res.status(422).json({ message: '"level" must be a number' });
-  }
-  if (level < 1) {
-    const teste = '"level" must be greater than or equal to 1';
-    return res.status(422).json({ message: teste });
-  }
-        
+  }        
   next();
 }
